6dff2f6cd7381c28f7dfe298d7ac0157f4cd9c0a
Ailerssi <danila.kuratnik1@gmail.com>
2023-04-03 19:56:49 +0300

    DZ_Kuratnik_JS1

---
+++ scripts/script.js
@@ -0,0 +1,69 @@
+// Задание 2
+let a1 = 5 % 3;
+let a2 = 3 % 5 ;
+let a3 = 5 + '3';
+let a4 = '5' - 3;
+let a5 = 75 + 'кг';
+let a6 = 785 * 653;
+let a7 = 100 / 25;
+let a8 = 0 * 0;
+let a9 = 0 / 2;
+let a10 = 89 / 0;
+let a11 = 98 + 2;
+let a12 = 5 - 98;
+let a13 = (8 + 56 * 4) / 5;
+let a14 = (9 - 12) * 7 / (5 + 2);
+let a15 = + '123';
+let a16 = 1 || 0;
+let a17 = false || true;
+let a18 = true > 0;
+console.log(typeof(a1));
+console.log(typeof(a2));
+console.log(typeof(a3));
+console.log(typeof(a4));
+console.log(typeof(a5));
+console.log(typeof(a6));
+console.log(typeof(a7));
+console.log(typeof(a8));
+console.log(typeof(a9));
+console.log(typeof(a10));
+console.log(typeof(a11));
+console.log(typeof(a12));
+console.log(typeof(a13));
+console.log(typeof(a14));
+console.log(typeof(a15));
+console.log(typeof(a16));
+console.log(typeof(a17));
+console.log(typeof(a18));
+
+// задание 3
+let widht = 10;
+let height = 22;
+let result = 'Ответ';
+
+console.log(result + " " + widht * height)
+
+// задание 4
+let h = 10;
+let VCilindra = Math.PI * a7 * h;
+console.log(VCilindra)
+
+// задание 5 
+let rad = 10;
+let sKruga = Math.PI * Math.pow(rad , 2);
+console.log(sKruga)
+
+// Задание 6
+let a = 5;
+let b = 7;
+let h1 = 10;
+let STrap = (a + b) / 2 * h1;
+console.log(STrap)
+
+// Задание 7
+let s = 2000000
+let p = 0.1
+let years = 5;
+let perep = s * p * years 
+console.log(perep)
+
---
+++ scripts/script1.js
@@ -0,0 +1,13 @@
+// Решено ChatGPT
+
+const q = 8;
+const t = 3;
+
+let x1 = (16 - q - 2 * t) / 2;
+console.log("x1 =", x1);
+
+let x2 = (q - t * 15) / (t - 6);
+console.log("x2 =", x2);
+
+let x3 = (23780) / (1 + 2 + q + t);
+console.log("x3 =", x3);
